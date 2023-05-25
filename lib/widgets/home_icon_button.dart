28517913e508c5812551b739adb009516e942c7f
BigAchiever <danishali9575@gmail.com>
2023-05-25 21:43:35 +0530

    reusable widget for icon button of home screen

---
+++ lib/widgets/home_icon_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:play_real/screens/home.dart';
+
+import '../colors/theme/themedata.dart';
+
+class CustomIconButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Widget child;
+
+  const CustomIconButton({
+    required this.onPressed,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width / 8,
+      child: FloatingActionButton(
+        onPressed: onPressed,
+        elevation: 0,
+        backgroundColor: StartingScreenState.lightmodedarkmode
+            ? lightCommonButton2
+            : CommonButton2,
+        shape: CircleBorder(
+          side: BorderSide(
+            color: Colors.black,
+            width: 3,
+          ),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
