d6589e68b8edf4edc2b7a1767320ab48487f235c
BigAchiever <danishali9575@gmail.com>
2023-05-28 02:43:53 +0530

    fixes

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,8 +1,6 @@
 import 'package:appwrite/appwrite.dart';
 
+
 Client client = Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') 
-    .setProject('6471b5261763b0d1f3d1');         
-
-
-
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('6471b5261763b0d1f3d1');
