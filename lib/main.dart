8df70850a5356351c17d95930c5233c0784ab6b8
BigAchiever <danishali9575@gmail.com>
2023-05-29 00:53:54 +0530

    Landscape mode disabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:play_real/provvider/player_age_provider.dart';
 import 'package:provider/provider.dart';
 import 'appwrite/auth.dart';
 import 'screens/home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(
     const MyApp(),
   );
