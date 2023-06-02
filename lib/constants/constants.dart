957bce5b678b16011ec184033d84fa75cc18b7c8
BigAchiever <danishali9575@gmail.com>
2023-06-02 20:07:30 +0530

    initial

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,16 +1,12 @@
 import 'package:appwrite/appwrite.dart';
 
 final client = Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('6471b5261763b0d1f3d1')
+    .setEndpoint('PROJECT_ENDPOINT')
+    .setProject('PROJECT_ID')
     .setSelfSigned();
 
 final databases = Databases(client);
 
-final document = databases.createDocument(
-    databaseId: '64777457efa83eda9359',
-    documentId: ID.unique(),
-    data: {},
-    collectionId: '647774e068f032e52a51');
+
 
 const mobileScreenWidth = 450.0;
